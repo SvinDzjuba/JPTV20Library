e03545f35791f7ae06eab28a77fc17b0000e1a66
pupil <pupil@ivkhk.ee>
2021-10-07 14:41:01 +0300

    ff

--- src/jptv20library/App.java
+++ src/jptv20library/App.java
@@ -7,21 +7,37 @@ import myclasses.Book;
 
 public class App {
     Scanner scanner = new Scanner(System.in);
+    private Book[] books = new Book[10];
     public void run() {
         String repeat = "r";
         do{
             System.out.println("Задача - 0: Выход из программы");
-            System.out.println("Задача - 1: Информация об авторе и книге");
+            System.out.println("Задача - 1: Добавить книгу");
+            System.out.println("Задача - 2: Список книг");
             System.out.printf("Выберите номер задачи: ");
-            int task = scanner.nextInt();
+            int task = scanner.nextInt();scanner.nextLine();
             switch (task) {
                 case 0:
                     repeat = "d";
                     System.out.println("Пока!");
                     break;
                 case 1:
-                    System.out.println("Добавляем книгу");
-                    addBook();
+                    System.out.println("---- Добавление книги ----");
+                    for (int i = 0; i < books.length; i++) {
+                        if(books[i] == null){
+                            books[i] = addBook();
+                            break;
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.println("---- Список книг -----");
+                    for (int i = 0; i < books.length; i++) {
+                        if(books[i] != null){
+                            System.out.println(books[i].toString());
+                        }
+                    }
+                    break;
                 default:
                     System.out.println("Введите номер из списка: ");
             }
@@ -30,22 +46,28 @@ public class App {
     }
         private Book addBook(){
             Book book = new Book();
-            System.out.println("Введите название книги: ");
+            System.out.printf("Введите название книги: ");
             book.setBookName(scanner.nextLine());
-            System.out.println("Введите количество авторов этой книги: ");
-            int countAuthors = scanner.nextInt(); scanner.nextLine();  
-            System.out.println("Введите автора книги: ");
-            System.out.println("Введите дату издания книги: ");
+            System.out.println("");
+            System.out.printf("Введите дату издания книги: ");
             book.setPublishedYear(scanner.nextInt());
-            Author [] authors = new Author[countAuthors];  
+            System.out.println("");
+            System.out.printf("Введите количество авторов: ");
+            int countAuthors = scanner.nextInt(); scanner.nextLine(); 
+            Author [] authors = new Author[countAuthors]; 
+            System.out.println("=================================");
             for(int i = 0; i < authors.length; i++){
                 Author author = new Author();
-                System.out.println("Введите имя автора: ");
+                System.out.printf("Введите имя автора: ");
                 author.setFirstName(scanner.nextLine());
-                System.out.println("Введите фамилию автора: ");
+                System.out.println("");
+                System.out.printf("Введите фамилию автора: ");
                 author.setLastName(scanner.nextLine());
-                System.out.println("Введите год рождения автора: ");
-                author.setYear(scanner.nextInt());
+                System.out.println("");
+                System.out.printf("Введите год рождения автора: ");
+                author.setYear(scanner.nextInt());scanner.nextLine();
+                System.out.println("");
+                System.out.println("===============================");
             }
             book.setAuthor(authors);
             return book;
