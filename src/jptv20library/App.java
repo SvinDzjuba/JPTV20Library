a17055353d2a329ad10f9e719fd7cbcfde5f6364
pupil <pupil@ivkhk.ee>
2021-10-07 15:09:32 +0300

    ff

--- src/jptv20library/App.java
+++ src/jptv20library/App.java
@@ -1,19 +1,27 @@
 package jptv20library;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 import myclasses.Author;
 import myclasses.Book;
+import myclasses.History;
+import myclasses.Reader;
 
 
 public class App {
     Scanner scanner = new Scanner(System.in);
     private Book[] books = new Book[10];
+    private Reader[] reader = new Reader[10];
+    Calendar c = new GregorianCalendar();
     public void run() {
         String repeat = "r";
         do{
             System.out.println("Задача - 0: Выход из программы");
             System.out.println("Задача - 1: Добавить книгу");
             System.out.println("Задача - 2: Список книг");
+            System.out.println("Задача - 3: Ввод информации о читателе");
+            System.out.println("Задача - 4: Информация о читателе");
             System.out.printf("Выберите номер задачи: ");
             int task = scanner.nextInt();scanner.nextLine();
             switch (task) {
@@ -35,6 +43,22 @@ public class App {
                     for (int i = 0; i < books.length; i++) {
                         if(books[i] != null){
                             System.out.println(books[i].toString());
+                        }
+                    }
+                    break;
+                case 3:
+                    System.out.println("---- Ввод информация о читателе ----");
+                    for (int i = 0; i < reader.length; i++) {
+                        if(reader[i] == null){
+                            reader[i] = addReader();
+                            break;
+                        }
+                    }
+                case 4:
+                    System.out.println("---- Читатель -----");
+                    for (int i = 0; i < reader.length; i++) {
+                        if(reader[i] != null){
+                            System.out.println(reader[i].toString());
                         }
                     }
                     break;
@@ -73,8 +97,28 @@ public class App {
             return book;
         
         }
+        private Reader addReader(){
+            Reader reader = new Reader();
+            System.out.printf("Введите имя читателя: ");
+            reader.setFirstName(scanner.nextLine());
+            System.out.println("");
+            System.out.printf("Введите фамилию читателя: ");
+            reader.setLastName(scanner.nextLine());
+            System.out.println("");
+            System.out.printf("Введите номер телефона читателя: ");
+            reader.setPhone(scanner.nextLine());
+            System.out.println("=================================");
+            
+            return reader;
         
-}
+ 
+        }
+        
+        }
+        
+        
+
+
 
 
 
